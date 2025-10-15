7c2b99ae1600e79552e2ecea91d543871efed642
Kanav <modikanav9@gmail.com>
2025-10-15 15:21:48 +0530

    Deleted Folder

--- backend/Kavya_Space/routes/electiveRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createElective, getAllElectives } from "../controllers/electiveController.js";
-
-const router = express.Router();
-
-router.post("/", createElective);
-router.get("/", getAllElectives);
-
-export default router;
